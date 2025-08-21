557426bd30ab2899941059fae21e650f7514c4d9
Nicolle Mikhelle de Sousa Andrade <2025114000036@pcs.ifsuldeminas.local>
2025-08-21 16:04:21 -0300

    adicionando codigos ao repertorio

---
+++ 1.introducao/hello.js
@@ -0,0 +1,4 @@
+
+console.log("\nhello word\n");
+
+console.log("----------")
---
+++ 1.introducao/operadores.js
@@ -0,0 +1,10 @@
+let x = 1;
+let y  = 2;
+
+console.log("x = 1 & y = 2");
+console.log(1+2);
+console.log("soma:" + (x+y));
+console.log("subtração:" + (x-y));
+console.log("multiplicação" + (x*y));
+console.log("divisão;"(x/y));
+console.log("--------")
